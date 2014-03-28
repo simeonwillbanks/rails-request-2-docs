021cceed945df7d369aebb2bcd84f16710ff791b
Simeon F. Willbanks <sfw@simeonfosterwillbanks.com>
2014-03-28 15:35:53 -0700

    Hello, Rails!

--- blog/config/routes.rb
+++ blog/config/routes.rb
@@ -1,5 +1,4 @@
 Blog::Application.routes.draw do
-  get "welcome/index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -54,4 +53,6 @@ Blog::Application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
+
+  root to: "welcome#index"
 end
