2aa2aa3bc369107a9526de5706afd37fd745caf8
Simeon F. Willbanks <sfw@simeonfosterwillbanks.com>
2014-04-16 18:50:35 -0700

    Add examples, notes and questions on doc links

--- blog/lib/middleware/trace_point.rb
+++ blog/lib/middleware/trace_point.rb
@@ -12,6 +12,44 @@ module Middleware
 
         all << "#{tp.defined_class} #{tp.method_id} #{tp.lineno} #{tp.path}\n"
 
+        # Ruby EXAMPLE
+        #
+        # Fixnum to_s 59 /Users/simeon/.rbenv/versions/2.1.1/lib/ruby/2.1.0/securerandom.rb
+        #
+        # http://www.omniref.com/?q=Fixnum##to_s
+        # http://www.omniref.com/ruby/2.1.1/classes/Fixnum##to_s
+        # http://ruby-doc.org/core-2.1.1/Fixnum.html#method-i-to_s
+
+        # Ruby Stdlib EXAMPLE
+        # Ruby 2.1.1 sha1 1980b4d4e4cc1dfd7f04d88c03e9f0a60dd4e94e
+        #
+        # Mutex_m mu_synchronize 72 /Users/simeon/.rbenv/versions/2.1.1/lib/ruby/2.1.0/mutex_m.rb
+        #
+        # http://www.omniref.com/?q=Mutex_m##mu_synchronize
+        # http://www.omniref.com/ruby/2.1.1/classes/Mutex_m##mu_synchronize
+        # http://ruby-doc.org/stdlib-2.1.1/libdoc/mutex_m/rdoc/Mutex_m.html#method-i-mu_synchronize
+        # https://github.com/ruby/ruby/blob/1980b4d4e4cc1dfd7f04d88c03e9f0a60dd4e94e/lib/mutex_m.rb#L72
+
+        # Rails EXAMPLE
+        # Rails 4.0.4 sha1 2abe4b032d080f7177c6f2e34c9124c468e8a293
+        #
+        # ActionDispatch::RequestId call 19 /Users/simeon/.rbenv/versions/2.1.1/lib/ruby/gems/2.1.0/gems/actionpack-4.0.4/lib/action_dispatch/middleware/request_id.rb
+        #
+        # http://www.omniref.com/?q=ActionDispatch%3A%3ARequestId##call
+        # http://www.omniref.com/ruby/gems/actionpack/4.0.4/classes/ActionDispatch::RequestId##call
+        # http://api.rubyonrails.org/classes/ActionDispatch/RequestId.html#method-i-call
+        # https://github.com/rails/rails/blob/2abe4b032d080f7177c6f2e34c9124c468e8a293/actionpack/lib/action_dispatch/middleware/request_id.rb#L19
+
+
+        # NOTES
+        # - omniref search links have sidebar with other possible matches
+
+        # QUESTIONS
+        # - What to do about class methods?
+        #
+        # http://www.omniref.com/?q=OpenURI#.open_uri
+
+
         stats[tp.defined_class] ||= {}
         stats[tp.defined_class][tp.method_id] ||= 0
         stats[tp.defined_class][tp.method_id] += 1
